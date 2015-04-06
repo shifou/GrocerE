c6d1ebf10f5f8e26d0b6bc3646e522fac657430e
mlx <maolixun@gmail.com>
2015-04-06 17:44:45 -0400

    create the user table and store table

--- p2p/src/main/test_db.go
+++ p2p/src/main/test_db.go
@@ -9,14 +9,15 @@ import (
 )
 
 type User struct {
-	mid    string
-	ipaddr string
-	port   int
-	stores []string
+	Mid    string
+	Ipaddr string
+	Port   int
+	Stores string
 }
-type Users struct {
-	UserId int
-	Uname  string
+
+type Store struct {
+	Name string
+	List string
 }
 
 var logger *log.Logger
@@ -40,30 +41,30 @@ func main() {
 	logger.Println("==========start===========")
 	db, err := sql.Open("sqlite3", "./data.db")
 	defer db.Close()
-	sql := `create table users (userId integer, uname text);`
+	sql := `create table users (Mid text primary key, Ipaddr text, Port Integer, Stores text);`
 	_, err = db.Exec(sql)
 	if err != nil {
-		sql := `drop table users;create table users (userId integer, uname text);`
+		sql := `drop table users;create table users (Mid text primary key, Ipaddr text, Port Integer, Stores text);`
 		_, err = db.Exec(sql)
 		checkErr(sql, err)
 	}
-	sql = `insert into users(userId,uname) values(1,'Mike');`
+	sql = `insert into users(Mid,Ipaddr,Port,Stores) values(1,'127.0.0.1',80,"Giant Eagle,Best Buy");`
 	_, err = db.Exec(sql)
 	checkErr(sql, err)
-	sql = `insert into users(userId,uname) values(2,'Mike');`
+	sql = `insert into users(Mid,Ipaddr,Port,Stores) values(22,'127.0.0.44',80,"Giant Eagle,Best Buy");`
 	_, err = db.Exec(sql)
 	checkErr(sql, err)
-	sql = `insert into users(userId,uname) values(3,'MMMMMMMike');`
+	sql = `insert into users(Mid,Ipaddr,Port,Stores) values(2555,'127.0.0.55',80,"ABC,DEF");`
 	_, err = db.Exec(sql)
 	checkErr(sql, err)
 	sql = `select * from users;`
 	rows, err := db.Query(sql)
 	checkErr(sql, err)
 	defer rows.Close()
-	var users []Users = make([]Users, 0)
+	var users []User = make([]User, 0)
 	for rows.Next() {
-		var u Users
-		rows.Scan(&u.UserId, &u.Uname)
+		var u User
+		rows.Scan(&u.Mid, &u.Ipaddr, &u.Port, &u.Stores)
 		users = append(users, u)
 	}
 	fmt.Println(users)
